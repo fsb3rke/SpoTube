a0114c3a0814ed61fa56170e234a877f16664a03
fsb3rke <berkeeavcii@gmail.com>
2023-06-28 00:29:07 +0300

    Added threading module. Added handle_insert method for threading. Added inserted track name and (inserted track count / total). Renamed Ok button name to Convert button name. Added Stop button but it's unworking for now.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,22 @@ from youtube import Youtube, Playlist
 from spotify import Spotify
 import youtubesearchpython
 import PySimpleGUI as sg
+from threading import Thread
+
+
+def handle_insert(pl: Playlist, track_fetch_data, window):
+    ll = len(track_fetch_data)
+    for i, track in enumerate(track_fetch_data):
+        video_id = youtubesearchpython.VideosSearch(f"{track[0]} {track[1]}", limit=1).result()["result"][0]["id"]
+        pl.insert(video_id)
+        window["inserted_track_text"].update(f"{track[0]} | {track[1]}")
+        window["total_inserted_track_text"].update(f" ({(i+1)}/{ll})")
+        print(f"{track[0]} | {track[1]} => {video_id} ? INSERTED | ({(i+1)}/{ll})")
+        
+        # global stop_thread
+        # if stop_thread:
+            # break
+
 
 def main():
     yt: Youtube = Youtube()
@@ -13,26 +29,33 @@ def main():
         [sg.Text("SpoTube")],
         [sg.Text("Youtube Playlist Id"), sg.InputText()],
         [sg.Text("Spotify Playlist Id"), sg.InputText()],
-        [sg.Button("Ok")]
+        [sg.Button("Convert")], #, sg.Button("Stop")],
+        [sg.Text("fsb3rke", key="inserted_track_text"), sg.Text("0/0", key="total_inserted_track_text")]
     ]
 
     window = sg.Window("SpoTube", layout)
 
     while True:
         event, values = window.read()
+        th = None
+
         if event == sg.WIN_CLOSED:
             break
 
-        if event == "Ok":
+        if event == "Convert":
             if not (values[0] == "" or values[1] == ""):
                 pl: Playlist = Playlist(yt.youtube, values[0])
                 track_fetch_data = sp.fetch_playlist_items_name_with_artist_name(values[1])
 
-                for track in track_fetch_data:
-                    # Search name in Youtube
-                    video_id = youtubesearchpython.VideosSearch(f"{track[0]} {track[1]}", limit=1).result()["result"][0]["id"]
-                    pl.insert(video_id=video_id)
-                    print(f"{video_id} has been inserted")
+                # stop_thread = False
+                th = Thread(target=handle_insert, args=(pl, track_fetch_data, window))
+                th.start()
+
+        # if event == "Stop":
+            # if th is not None:
+                # stop_thread = True
+                # th.join()
+                # th = None
 
             print(values)
 
