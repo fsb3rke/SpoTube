3d3b2d36d8fa855dcc1d505b8bd6ee2dd3469fe7
fsb3rke <berkeeavcii@gmail.com>
2023-06-28 16:08:50 +0300

    Added start.sh for run program easily. Added max number of insert track input and sets for it

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 from threading import Thread
 
 
-def handle_insert(pl: Playlist, track_fetch_data, window):
+def handle_insert(pl: Playlist, track_fetch_data, window, max_number_of_insert_track):
     ll = len(track_fetch_data)
     for i, track in enumerate(track_fetch_data):
         video_id = youtubesearchpython.VideosSearch(f"{track[0]} {track[1]}", limit=1).result()["result"][0]["id"]
@@ -13,6 +13,9 @@ def handle_insert(pl: Playlist, track_fetch_data, window):
         window["inserted_track_text"].update(f"{track[0]} | {track[1]}")
         window["total_inserted_track_text"].update(f" ({(i+1)}/{ll})")
         print(f"{track[0]} | {track[1]} => {video_id} ? INSERTED | ({(i+1)}/{ll})")
+
+        if (i+1) == max_number_of_insert_track:
+            break
         
         # global stop_thread
         # if stop_thread:
@@ -29,6 +32,7 @@ def main():
         [sg.Text("SpoTube")],
         [sg.Text("Youtube Playlist Id"), sg.InputText()],
         [sg.Text("Spotify Playlist Id"), sg.InputText()],
+        [sg.Text("Max Number of Insert Track"), sg.InputText()],
         [sg.Button("Convert")], #, sg.Button("Stop")],
         [sg.Text("fsb3rke", key="inserted_track_text"), sg.Text("0/0", key="total_inserted_track_text")]
     ]
@@ -37,18 +41,22 @@ def main():
 
     while True:
         event, values = window.read()
+        youtube_playlist_id, spotify_playlist_id, max_number_of_insert_track = values[0], values[1], values[2]
         th = None
 
         if event == sg.WIN_CLOSED:
             break
 
         if event == "Convert":
-            if not (values[0] == "" or values[1] == ""):
+            if not (youtube_playlist_id == "" or spotify_playlist_id == ""):
                 pl: Playlist = Playlist(yt.youtube, values[0])
                 track_fetch_data = sp.fetch_playlist_items_name_with_artist_name(values[1])
+                int_max_number_of_insert_track = int(max_number_of_insert_track)
+                int_max_number_of_insert_track = len(track_fetch_data) if int_max_number_of_insert_track > len(track_fetch_data) else 1 if int_max_number_of_insert_track < 1 else int_max_number_of_insert_track
+
 
                 # stop_thread = False
-                th = Thread(target=handle_insert, args=(pl, track_fetch_data, window))
+                th = Thread(target=handle_insert, args=(pl, track_fetch_data, window, int_max_number_of_insert_track))
                 th.start()
 
         # if event == "Stop":
@@ -57,7 +65,7 @@ def main():
                 # th.join()
                 # th = None
 
-            print(values)
+            print(values) # , stop_thread)
 
 
 
