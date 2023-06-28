20793d83124ea91d59d21b24424dbe23052e034f
fsb3rke <berkeeavcii@gmail.com>
2023-06-28 23:37:24 +0300

    Added character limit for block resize bugs. Added some methods for check is_empty or slice characters of track[0]. Added Raiser class for global stop_thread variable. Added pl, track_fetch_data, ll, int_max_number_of_insert_track variables into handle_convert method from Convert event. Changed th = None to th = [None] for change directly address.

--- src/main.py
+++ src/main.py
@@ -5,24 +5,38 @@ import PySimpleGUI as sg
 from threading import Thread
 
 
-stop_thread = [False] # manipulate pointer
+MAX_CHARACTER_LIMIT = 40
 
-def handle_insert(pl: Playlist, track_fetch_data, window, max_number_of_insert_track):
+def is_empty(x: str) -> bool:
+    return x == ""
+
+def handle_character_limit(x: str) -> str:
+    return x[:MAX_CHARACTER_LIMIT]
+
+class Raiser:
+    def __init__(self):
+        self.stop_thread = False
+
+def handle_convert(window, values, sp, yt, max_number_of_insert_track, raiser) -> None:
+    pl: Playlist = Playlist(yt.youtube, values[0])
+    track_fetch_data = sp.fetch_playlist_items_name_with_artist_name(values[1])
     ll = len(track_fetch_data)
+    int_max_number_of_insert_track = int(max_number_of_insert_track)
+    int_max_number_of_insert_track = ll if int_max_number_of_insert_track > ll else 1 if int_max_number_of_insert_track < 1 else int_max_number_of_insert_track
+
+
     for i, track in enumerate(track_fetch_data):
         video_id = youtubesearchpython.VideosSearch(f"{track[0]} {track[1]}", limit=1).result()["result"][0]["id"]
         pl.insert(video_id)
-        window["inserted_track_text"].update(f"{track[0]} | {track[1]}")
-        window["total_inserted_track_text"].update(f" ({(i+1)}/{ll})")
-        print(f"{track[0]} | {track[1]} => {video_id} ? INSERTED | ({(i+1)}/{ll})")
+        window["inserted_track_text"].update(f"{handle_character_limit(track[0])} | {track[1]}")
+        window["total_inserted_track_text"].update(f" ({(i+1)}/{int_max_number_of_insert_track})")
+        print(f"{track[0]} | {track[1]} => {video_id} ? INSERTED | ({(i+1)}/{int_max_number_of_insert_track})")
 
-        if (i+1) == max_number_of_insert_track:
+        if (i+1) >= int(max_number_of_insert_track): # Why this is happening idk. It recongnized str instead of int.
             break
         
-        
-        if stop_thread[0]:
+        elif raiser.stop_thread:
             break
-
 
 def main():
     yt: Youtube = Youtube()
@@ -44,28 +58,31 @@ def main():
     while True:
         event, values = window.read()
         youtube_playlist_id, spotify_playlist_id, max_number_of_insert_track = values[0], values[1], values[2]
-        th = None
+        th = [None]
+        raiser = Raiser()
 
         if event == sg.WIN_CLOSED:
             break
 
         if event == "Convert":
-            if not (youtube_playlist_id == "" or spotify_playlist_id == ""):
-                pl: Playlist = Playlist(yt.youtube, values[0])
-                track_fetch_data = sp.fetch_playlist_items_name_with_artist_name(values[1])
-                int_max_number_of_insert_track = int(max_number_of_insert_track)
-                int_max_number_of_insert_track = len(track_fetch_data) if int_max_number_of_insert_track > len(track_fetch_data) else 1 if int_max_number_of_insert_track < 1 else int_max_number_of_insert_track
+            if not (is_empty(youtube_playlist_id) or is_empty(spotify_playlist_id)):
 
-
-                # stop_thread = False
-                th = Thread(target=handle_insert, args=(pl, track_fetch_data, window, int_max_number_of_insert_track))
-                th.start()
+                try:
+                    th[0] = Thread(target=handle_convert, args=(window, values, sp, yt, max_number_of_insert_track, raiser))
+                    th[0].start()
+                except RuntimeError:
+                    pass
+                
+                raiser.stop_thread = False
 
         if event == "Stop":
-            if th is not None:
-                stop_thread[0] = True
-                th.join()
-                th = None
+            try:
+                print("sa")
+                raiser.stop_thread = True
+                th[0].join()
+                raiser.stop_thread = False # TODO: Solve bug : RuntimeError
+            except RuntimeError:
+                pass
 
             print(values) # , stop_thread)
 
