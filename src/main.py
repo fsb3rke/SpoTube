0d3596cdb125856a4e92b1fe3f89cf1311afed33
fsb3rke <berkeeavcii@gmail.com>
2023-06-29 19:30:52 +0300

    Add checkbox for max number of insert track into youtube playlist.

--- src/main.py
+++ src/main.py
@@ -17,13 +17,13 @@ class Raiser:
     def __init__(self):
         self.stop_thread = False
 
-def handle_convert(window, values, sp, yt, max_number_of_insert_track, raiser) -> None:
+def handle_convert(window, values, sp, yt, max_number_of_insert_track, raiser, checkbox_dict) -> None:
     pl: Playlist = Playlist(yt.youtube, values[0])
     window["inserted_track_text"].update("Fetching Spotify Playlist...")
     track_fetch_data = sp.fetch_playlist_items_name_with_artist_name(values[1])
     ll = len(track_fetch_data)
     int_max_number_of_insert_track = int(max_number_of_insert_track)
-    int_max_number_of_insert_track = ll if int_max_number_of_insert_track > ll else 1 if int_max_number_of_insert_track < 1 else int_max_number_of_insert_track
+    int_max_number_of_insert_track = ll if checkbox_dict["insert_track_max"] else ll if int_max_number_of_insert_track > ll else 1 if int_max_number_of_insert_track < 1 else int_max_number_of_insert_track
 
     try:
         for i, track in enumerate(track_fetch_data):
@@ -49,9 +49,9 @@ def main():
 
     layout = [
         [sg.Text("SpoTube")],
-        [sg.Text("Youtube Playlist Id"), sg.Push(), sg.InputText()],
+        [sg.Text("Youtube Playlist Id"), sg.Push(), sg.InputText()],# sg.Checkbox("create", key="create_youtube_playlist", default=False), sg.InputText()],
         [sg.Text("Spotify Playlist Id  "), sg.Push(), sg.InputText()],
-        [sg.Text("Max Number of Insert Track"), sg.InputText()],
+        [sg.Text("Max Number of Insert Track"), sg.Checkbox("max", key="insert_track_max", default=True, enable_events=True), sg.InputText(disabled=True, key="insert_track_max_input")],
         [sg.Button("Convert"), sg.Button("Stop")],
         [sg.Text("fsb3rke", key="inserted_track_text"), sg.Push(), sg.Text("0/0", key="total_inserted_track_text")]
     ]
@@ -59,23 +59,25 @@ def main():
     window = sg.Window("SpoTube", layout)
     th = [None]
     raiser = Raiser()
+    checkbox_dict: dict = {"insert_track_max": True}
 
     while True:
         event, values = window.read()
-        youtube_playlist_id, spotify_playlist_id, max_number_of_insert_track = values[0], values[1], values[2]
+        # youtube_playlist_id, spotify_playlist_id, max_number_of_insert_track = values[0], values[1], values[2]
 
         if event == sg.WIN_CLOSED:
             break
 
         if event == "Convert":
-            if not (is_empty(youtube_playlist_id) or is_empty(spotify_playlist_id)):
+            if not (is_empty(values[0]) or is_empty(values[1])):
 
-                th[0] = Thread(target=handle_convert, args=(window, values, sp, yt, max_number_of_insert_track, raiser))
+                th[0] = Thread(target=handle_convert, args=(window, values, sp, yt, values[2], raiser, checkbox_dict))
                 th[0].start()
                 
                 raiser.stop_thread = False
 
         if event == "Stop":
+            # TODO: Handle this with another thread
             print("Stop Button Pressed")
             raiser.stop_thread = True
             th[0].join()
@@ -84,6 +86,12 @@ def main():
             window["total_inserted_track_text"].update("0/0")
 
             print(values) # , stop_thread)
+
+        if event == "insert_track_max":
+            checkbox_dict["insert_track_max"] = not checkbox_dict["insert_track_max"]
+            window["insert_track_max"].update(value=checkbox_dict["insert_track_max"])
+            window["insert_track_max_input"].update(disabled=(not checkbox_dict["insert_track_max"]))
+            print(checkbox_dict["insert_track_max"], (not checkbox_dict["insert_track_max"]))
 
 
 
